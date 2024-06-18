a88e4b66289866c14b5a7725d6cfa77bd0659e1f
tongsantan <vincente53@outlook.com>
2024-06-18 08:26:55 +0800

    updated

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -11,14 +11,6 @@ from src.utils import save_object,evaluate_models
 import warnings
 warnings.filterwarnings("ignore")
 
-from sklearn.tree import DecisionTreeRegressor
-
-from sklearn.ensemble import (RandomForestRegressor,
-                              AdaBoostRegressor,
-                              GradientBoostingRegressor)
-
-from sklearn.linear_model import LinearRegression
-
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path=os.path.join("output","model.pkl")
@@ -39,41 +31,13 @@ class ModelTrainer:
             )
             models = {
                 
-                    "Random Forest": RandomForestRegressor(),
-                    "Decision Tree": DecisionTreeRegressor(),
-                    "Gradient Boosting": GradientBoostingRegressor(),
-                    "Linear Regression": LinearRegression(),
                     "Hist Gradient Boosting": HistGradientBoostingRegressor(),
                     "XGBRegressor": XGBRegressor(),
                     "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                    "AdaBoost Regressor": AdaBoostRegressor(),
+
                 }
             params={
                     
-                    "Decision Tree": {
-                        'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                        # 'splitter':['best','random'],
-                        # 'max_features':['sqrt','log2'],
-                                        },
-                
-                        "Random Forest":{
-                        # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                            
-                        # 'max_features':['sqrt','log2',None],
-                        'n_estimators': [8,16,32,64,128,256]
-                                        },
-                
-                        "Gradient Boosting":{
-                        # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                        'learning_rate':[.1,.01,.05,.001],
-                        'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                        # 'criterion':['squared_error', 'friedman_mse'],
-                        # 'max_features':['auto','sqrt','log2'],
-                        'n_estimators': [8,16,32,64,128,256]
-                                            },
-                
-                        "Linear Regression":{},
-                
                         "Hist Gradient Boosting":{
                         'learning_rate':[.1,.01,.05,.001],
                         'max_depth': [6,8,10]
@@ -89,12 +53,6 @@ class ModelTrainer:
                         'learning_rate': [0.01, 0.05, 0.1],
                         'iterations': [30, 50, 100]
                                                 },  
-                
-                        "AdaBoost Regressor":{
-                        'learning_rate':[.1,.01,0.5,.001],
-                        # 'loss':['linear','square','exponential'],
-                        'n_estimators': [8,16,32,64,128,256]
-                                            }
 
                 }
 
